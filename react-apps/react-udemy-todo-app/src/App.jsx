9d6967b92fca9735453fdf0bb453df4a9582f4c4
Hiroaki Makino <8makino.42tokyo@gmail.com>
2024-09-06 17:58:24 +0900

    🎉 First commit

---
+++ react-apps/react-udemy-todo-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { ColorfulMessage } from "./components/ColorfulMessage";
+
+export const App = () => {
+  const onClickButton = () => alert();
+  const contentStyleB = {
+    color: "green",
+    fontSize: "18px",
+  };
+
+  return (
+    <>
+      <h1 style={{color: "red"}}>こんにちは！</h1>
+      <ColorfulMessage color="blue" message="お元気ですか？" />
+      <p style={contentStyleB}>元気です！</p>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
