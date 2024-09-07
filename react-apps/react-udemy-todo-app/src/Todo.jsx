a0ed28d85f0b3af62dcb0a8fa40222b50d028ea6
Hiroaki Makino <8makino.42tokyo@gmail.com>
2024-09-07 09:26:28 +0900

    ✨ 追加ボタンの実装

--- react-apps/react-udemy-todo-app/src/Todo.jsx
+++ react-apps/react-udemy-todo-app/src/Todo.jsx
@@ -2,20 +2,57 @@ import { useState } from "react";
 import "./style.css";
 
 export const Todo = () => {
-  const [incompleteTodos, setIncompleteTodos] = useState([
-    "TODOです1",
-    "TODOです2",
-  ]);
+  const [todoText, setTodoText] = useState("");
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
   const [completeTodos, setCompleteTodos] = useState([
     "TODOでした1",
     "TODOでした2",
   ]);
 
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const validateInput = () => {
+    const trimedTodoText = todoText.trim();
+    const totalTodos = incompleteTodos.length + completeTodos.length + 1;
+    const MAX_TODO = 10;
+
+    if (trimedTodoText === "") {
+      return {
+        success: false,
+        message: "TODOを入力してください",
+      };
+    }
+    if (totalTodos > MAX_TODO) {
+      return {
+        success: false,
+        message:
+          "TODOリストに追加できるのは10個までです。項目を削除してください。",
+      };
+    }
+    return { success: true };
+  };
+
+  const onClickAdd = () => {
+    const result = validateInput();
+
+    if (!result.success) {
+      alert(result.message);
+      return;
+    }
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力"></input>
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        ></input>
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
